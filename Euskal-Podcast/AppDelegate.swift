7d578c5901053f06821576356bdefa3aad1c1191
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-05-02 00:29:57 +0200

    Changed the Name of the App from "Euskal-Podcast" to "Euskal Podcast". Updated the supported interface orientations and minimum iOS version. Created the "MainViewController" with its XIB and deleted the Main storyboard. Configured the project to work without the storyboard and updated AppDelegate and SceneDelegate.

--- Euskal-Podcast/AppDelegate.swift
+++ Euskal-Podcast/AppDelegate.swift
@@ -10,27 +10,42 @@ import UIKit
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-
+    var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        
+        // Create an instance of the Main VC.
+        let mainVC: MainViewController = MainViewController()
+        
+        // Create the Navigation Controller and add the Main VC to it.
+        let navigationController: UINavigationController = UINavigationController()
+        navigationController.pushViewController(mainVC, animated: false)
+        
+        // Create the window and add the Navigation Controller as the root view.
+        let frame = UIScreen.main.bounds
+        window = UIWindow(frame: frame)
+        if let window = window {
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
+        
         return true
     }
 
     // MARK: UISceneSession Lifecycle
-
+    @available (iOS 13, *)
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
         return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
     }
-
+    
+    @available (iOS 13, *)
     func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
         // Called when the user discards a scene session.
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
 
 }
 
--- Euskal-Podcast/SceneDelegate.swift
+++ Euskal-Podcast/SceneDelegate.swift
@@ -7,16 +7,30 @@
 
 import UIKit
 
+@available (iOS 13, *)
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        
+        // Create an instance of the Main VC.
+        let mainVC: MainViewController = MainViewController()
+        
+        // Create the Navigation Controller and add the Main VC to it.
+        let navigationController: UINavigationController = UINavigationController()
+        navigationController.pushViewController(mainVC, animated: false)
+        
+        // Create the window and add the Navigation Controller as the root view.
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        if let window = window {
+            window.windowScene = windowScene
+            window.rootViewController = navigationController
+            window.makeKeyAndVisible()
+        }
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
