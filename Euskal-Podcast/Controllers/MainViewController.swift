e849956c8cfd4129b9f73b99da2068f4431bb5bb
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2021-05-02 23:29:41 +0200

    Added a table view in the Main view controller. Added some code in Player view controller to play / stream an mp3 from a server.

--- Euskal-Podcast/Controllers/MainViewController.swift
+++ Euskal-Podcast/Controllers/MainViewController.swift
@@ -8,22 +8,50 @@
 import UIKit
 
 class MainViewController: UIViewController {
-
+    
+    // MARK: - UI Elements
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    // MARK: - Properties
+    
+    // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        self.setupTableView()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    
+    ///
+    ///
+    ///
+    private func setupTableView() {
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
     }
-    */
+}
 
+extension MainViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let playerVC: PlayerViewController = PlayerViewController()
+        self.navigationController?.pushViewController(playerVC, animated: true)
+    }
+}
+
+extension MainViewController: UITableViewDataSource {
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell()
+        cell.textLabel?.text = "Play"
+        return cell
+    }
+    
 }
--- Euskal-Podcast/Controllers/PlayerViewController.swift
+++ Euskal-Podcast/Controllers/PlayerViewController.swift
@@ -6,24 +6,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class PlayerViewController: UIViewController {
-
+    
+    // MARK: - UI Elements
+    
+    @IBOutlet weak var playPauseButton: UIButton!
+    @IBAction func playPauseButtonTapped(_ sender: Any) {
+        self.playPauseAction()
+    }
+    
+    // MARK: - Properties
+    
+    var player: AVPlayer?
+    var playerItem:AVPlayerItem?
+    var isPlaying: Bool = false
+    let urlString: String = "https://www.ivoox.com/miseriaren-adarrak-xenpelarren-bertso-sorta_mf_50074712_feed_1.mp3"
+    
+    // MARK: - Methods
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        self.playPauseButton.setTitle("Stop", for: .normal)
+        
+        self.setupPlayer()
     }
-
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    
+    private func setupPlayer() {
+        guard let url: URL = URL(string: self.urlString) else { return }
+        
+        let asset = AVAsset(url: url)
+        self.playerItem = AVPlayerItem(asset: asset)
+        self.player = AVPlayer(playerItem: playerItem)
+        
+        self.player?.volume = 1.0
     }
-    */
+    
+    ///
+    /// Play or Pause the media file.
+    ///
+    private func playPauseAction() {
+        if isPlaying {
+            self.playPauseButton.setTitle("Pause", for: .normal)
+            self.player?.pause()
+        } else {
+            self.playPauseButton.setTitle("Play", for: .normal)
+            self.player?.play()
+        }
+        
+        self.isPlaying.toggle()
+    }
 
 }
