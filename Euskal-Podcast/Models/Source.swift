bbb8af14eb16288931a771c48628da7216cb79d7
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2022-07-11 23:59:28 +0200

    Created the new model "Source".

---
+++ Euskal-Podcast/Models/Source.swift
@@ -0,0 +1,14 @@
+//
+//  Source.swift
+//  Euskal-Podcast
+//
+//  Created by Aitor Zubizarreta on 11/7/22.
+//
+
+import Foundation
+
+struct Source: Codable {
+    let Title: String
+    let IconURL: String
+    let JSONUrl: String
+}
