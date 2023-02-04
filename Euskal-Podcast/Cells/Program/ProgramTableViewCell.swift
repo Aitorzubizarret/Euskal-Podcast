abe70249d6df6f2e97e5de3765782e287f4f3b21
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2023-02-04 22:47:46 +0100

    Updated "ProgramTableViewCell" cell. Added some UI elements to display each Program data on a list.

--- Euskal-Podcast/Cells/Program/ProgramTableViewCell.swift
+++ Euskal-Podcast/Cells/Program/ProgramTableViewCell.swift
@@ -11,7 +11,11 @@ class ProgramTableViewCell: UITableViewCell {
     
     // MARK: - UI Elements
     
+    @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var bottomLineImageView: UIImageView!
     
     // MARK: - Properties
     
@@ -19,7 +23,19 @@ class ProgramTableViewCell: UITableViewCell {
         didSet {
             guard let receivedProgram = program else { return }
             
+            // Photo ImageView.
+            if let iconURL: URL = URL(string: receivedProgram.imageURL) {
+                photoImageView.kf.setImage(with: iconURL)
+            }
+            
+            // Title Label.
             titleLabel.text = receivedProgram.title
+            
+            // Description Label.
+            descriptionLabel.text = receivedProgram.description
+            
+            // Author Label.
+            authorLabel.text = receivedProgram.author
         }
     }
     
@@ -27,13 +43,29 @@ class ProgramTableViewCell: UITableViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        
+        setupView()
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+    
+    ///
+    /// Setup the View.
+    ///
+    private func setupView() {
+        // ImageView.
+        photoImageView.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
+        
+        photoImageView.layer.cornerRadius = 4
+        
+        photoImageView.layer.borderWidth = 1
+        photoImageView.layer.borderColor = UIColor.black.withAlphaComponent(0.1).cgColor
+        
+        bottomLineImageView.backgroundColor = UIColor.gray.withAlphaComponent(0.2)
     }
     
 }
