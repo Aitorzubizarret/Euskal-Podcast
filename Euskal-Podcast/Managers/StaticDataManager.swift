caa48a47680f08690238f327c63a51499229c367
Aitor Zubizarreta <aitorzubizarreta@yahoo.es>
2023-02-22 17:42:58 +0100

    Add new channels to StandardChannel list.

--- Euskal-Podcast/Managers/StaticDataManager.swift
+++ Euskal-Podcast/Managers/StaticDataManager.swift
@@ -122,7 +122,7 @@ final class StaticDataManager {
         channels.append(newChannel20)
         
         let newChannel21 = Channel()
-        newChannel21.name = "Hitza Jolas - EITB"
+        newChannel21.name = "Hitza jolas - EITB"
         newChannel21.urlAddress = "https://api.eitb.eus/api/eitbpodkast/getRss/396335897/itunes/"
         channels.append(newChannel21)
         
@@ -142,7 +142,7 @@ final class StaticDataManager {
         channels.append(newChannel24)
         
         let newChannel25 = Channel()
-        newChannel25.name = "KABALA - Klak"
+        newChannel25.name = "Kabala - Klak"
         newChannel25.urlAddress = "https://www.ivoox.com/feed_fg_f11381606_filtro_1.xml"
         channels.append(newChannel25)
         
@@ -150,6 +150,148 @@ final class StaticDataManager {
         newChannel26.name = "Solas Enea - Cristina Enea Fundazioa"
         newChannel26.urlAddress = "https://www.ivoox.com/feed_fg_f11397658_filtro_1.xml"
         channels.append(newChannel26)
+        
+        let newChannel27 = Channel()
+        newChannel27.name = "Zorionagurrak - Bizkaia Irratia"
+        newChannel27.urlAddress = "https://bizkaiairratia.eus/xml/zorionagurrak"
+        channels.append(newChannel27)
+        
+        let newChannel28 = Channel()
+        newChannel28.name = "Goizeko izarretan - Bizkaia Irratia"
+        newChannel28.urlAddress = "https://bizkaiairratia.eus/xml/goizeko-izarretan"
+        channels.append(newChannel28)
+        
+        let newChannel29 = Channel()
+        newChannel29.name = "Lau haizetara - Bizkaia Irratia"
+        newChannel29.urlAddress = "https://bizkaiairratia.eus/xml/lau-haizetara"
+        channels.append(newChannel29)
+        
+        let newChannel30 = Channel()
+        newChannel30.name = "Jirabiran - Bizkaia Irratia"
+        newChannel30.urlAddress = "https://bizkaiairratia.eus/xml/jirabiran"
+        channels.append(newChannel30)
+        
+        let newChannel31 = Channel()
+        newChannel31.name = "Jokoan - Bizkaia Irratia"
+        newChannel31.urlAddress = "https://bizkaiairratia.eus/xml/jokoan"
+        channels.append(newChannel31)
+        
+        let newChannel32 = Channel()
+        newChannel32.name = "Herriz herri - Bizkaia Irratia"
+        newChannel32.urlAddress = "https://bizkaiairratia.eus/xml/herriz-herri"
+        channels.append(newChannel32)
+        
+        let newChannel33 = Channel()
+        newChannel33.name = "Irakurrieran - Bizkaia Irratia"
+        newChannel33.urlAddress = "https://bizkaiairratia.eus/xml/irakurrieran"
+        channels.append(newChannel33)
+        
+        let newChannel34 = Channel()
+        newChannel34.name = "Albistekia - Asier Errasti"
+        newChannel34.urlAddress = "https://baleafunk.eus/api/v1/channels/albistekia/rss"
+        channels.append(newChannel34)
+        
+        let newChannel35 = Channel()
+        newChannel35.name = "Lehen LH Etxepare Lizeoa"
+        newChannel35.urlAddress = "https://baleafunk.eus/api/v1/channels/lehenlhetxeparelizeoa20222023/rss"
+        channels.append(newChannel35)
+        
+        let newChannel36 = Channel()
+        newChannel36.name = "BTN"
+        newChannel36.urlAddress = "https://baleafunk.eus/api/v1/channels/btn/rss"
+        channels.append(newChannel36)
+        
+        let newChannel37 = Channel()
+        newChannel37.name = "Oinherri"
+        newChannel37.urlAddress = "https://baleafunk.eus/api/v1/channels/oinherripodkastak/rss"
+        channels.append(newChannel37)
+        
+        let newChannel38 = Channel()
+        newChannel38.name = "Borroka, maitasun eta traizio istorioak - Argia"
+        newChannel38.urlAddress = "https://baleafunk.eus/api/v1/channels/borroka_maitasun_eta_traizio_istorioak/rss"
+        channels.append(newChannel38)
+        
+        let newChannel39 = Channel()
+        newChannel39.name = "Linuxtarrak garelako"
+        newChannel39.urlAddress = "https://baleafunk.eus/api/v1/channels/linuxtarrakgarelakopodcast/rss"
+        channels.append(newChannel39)
+        
+        let newChannel40 = Channel()
+        newChannel40.name = "Solasak"
+        newChannel40.urlAddress = "https://baleafunk.eus/api/v1/channels/solasak/rss"
+        channels.append(newChannel40)
+        
+        let newChannel41 = Channel()
+        newChannel41.name = "Eskilarapeko irratia - Ermuko Irrati Askea"
+        newChannel41.urlAddress = "https://baleafunk.eus/api/v1/channels/eskilarapeko_irratia/rss"
+        channels.append(newChannel41)
+        
+        let newChannel42 = Channel()
+        newChannel42.name = "Hezitzaile bat sarean"
+        newChannel42.urlAddress = "https://baleafunk.eus/api/v1/channels/hezitzailebatsarean/rss"
+        channels.append(newChannel42)
+        
+        let newChannel43 = Channel()
+        newChannel43.name = "Guraso.eus"
+        newChannel43.urlAddress = "https://baleafunk.eus/api/v1/channels/gurasoeus/rss"
+        channels.append(newChannel43)
+        
+        let newChannel44 = Channel()
+        newChannel44.name = "Gonagorri haur eta gazte literatura - Gonagorri Galtzagorri Elkartea"
+        newChannel44.urlAddress = "https://baleafunk.eus/api/v1/channels/gonagorri/rss"
+        channels.append(newChannel44)
+        
+        let newChannel45 = Channel()
+        newChannel45.name = "Ama(t)Asunak"
+        newChannel45.urlAddress = "https://baleafunk.eus/api/v1/channels/sabeletikmundura/rss"
+        channels.append(newChannel45)
+        
+        let newChannel46 = Channel()
+        newChannel46.name = "Podcastak.eus"
+        newChannel46.urlAddress = "https://baleafunk.eus/api/v1/channels/podcastakeus/rss"
+        channels.append(newChannel46)
+        
+        let newChannel47 = Channel()
+        newChannel47.name = "Xabildua"
+        newChannel47.urlAddress = "https://baleafunk.eus/api/v1/channels/xabilduapodcast/rss"
+        channels.append(newChannel47)
+        
+        let newChannel48 = Channel()
+        newChannel48.name = "Bestelakoak - Argia"
+        newChannel48.urlAddress = "https://baleafunk.eus/api/v1/channels/bestelakoak/rss"
+        channels.append(newChannel48)
+        
+        let newChannel49 = Channel()
+        newChannel49.name = "Puska dezagun izotza - Argia"
+        newChannel49.urlAddress = "https://baleafunk.eus/api/v1/channels/puskadezagunizotza/rss"
+        channels.append(newChannel49)
+        
+        let newChannel50 = Channel()
+        newChannel50.name = "Oasiko kronikak - Argia"
+        newChannel50.urlAddress = "https://baleafunk.eus/api/v1/channels/oasikokronikak/rss"
+        channels.append(newChannel50)
+        
+        let newChannel51 = Channel()
+        newChannel51.name = "ARGIAren podcastak - Argia"
+        newChannel51.urlAddress = "https://baleafunk.eus/api/v1/channels/argiarenpodcastak/rss"
+        channels.append(newChannel51)
+        
+        let newChannel52 = Channel()
+        newChannel52.name = "Beranduegi - Argia"
+        newChannel52.urlAddress = "https://baleafunk.eus/api/v1/channels/beranduegi/rss"
+        channels.append(newChannel52)
+        
+        let newChannel53 = Channel()
+        newChannel53.name = "Hodeia ez da existitzen"
+        newChannel53.urlAddress = "https://baleafunk.eus/api/v1/channels/hodeiaezdaexistitzen/rss"
+        channels.append(newChannel53)
+        
+        /*
+         let newChannel = Channel()
+         newChannel.name = ""
+         newChannel.urlAddress = ""
+         channels.append(newChannel)
+         */
     }
     
     private func saveChannelsInRealm() {
